2b2c7289b2b49812a00b1319ead8c6c379178b64
Manar Jber <mohamadmanar960@gmail.com>
2024-09-06 17:18:19 +0300

    feat: fix program structure

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,11 @@
-import express, { Request, Response } from "express";
-import os from 'os';
+import express from "express";
+import router from "./routes/userInfo.router";
+
 const app = express();
 const port = 3000;
 
-app.get("/api/hello", (req: Request, res: Response) => {
-  const name = req.query.name;
-  res.send({ greeting: `Hello, ${name || "world"}!` });
-});
+app.use(router);
 
-app.get("/info", (req: Request, res: Response) => {
-  const clientAddress = req.ip;
-  const hostName = os.hostname();
-  const requestTime = new Date().toISOString();
-
-  res.json({
-    time: requestTime,
-    client_address: clientAddress,
-    host_name: hostName,
-    headers: req.headers,
-  });
-});
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
